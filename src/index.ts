c56dcbdb66c3ae8dfd35d041a002b0f607a45416
ClowderDev <tranlevinhbuu@gmail.com>
2025-09-28 10:26:18 +0700

    feat: update index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-const name: string = 'ClowderDev'
-console.log(name)
+import express, { NextFunction, Request, Response } from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import { Env } from './config/env.config'
+import connectDatabase from '~/config/database.config'
+
+const app = express()
+
+connectDatabase()
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(Env.PORT, () => {
+  console.log(`Server is running on port ${Env.PORT}`)
+})
